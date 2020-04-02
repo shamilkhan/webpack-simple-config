d129078eeac82114d93a7c6d22213bd57845d50e
shamilkhan <shamilkhan1999@gmail.com>
2020-04-02 20:57:34 +0300

    fix app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 const App = () => <div>test</div>;
 
-ReactDOM.render(document.getElementById("root"), <App />);
+ReactDOM.render(<App />, document.getElementById("root"));
